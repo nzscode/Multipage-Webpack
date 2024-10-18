b0957aa71f7d62df493a42a22f65118bf5f34530
NZ <nzscode@gmail.com>
2024-10-18 17:05:18 -0400

    tested multipages

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
+const pages = ["index", "page1", "page2"];
+
 module.exports = {
-    entry: "./src/index.js",
-    plugins: [
-        new HtmlWebpackPlugin({
-            scriptLoading: "blocking",
-            template: "./src/index.html",
-        }),
-    ],
+    entry: pages.reduce((config, page) => {
+        config[page] = `./src/${page}.js`;
+        return config;
+    }, {}),
+
+    plugins: [].concat(
+        pages.map(
+            (page) =>
+                new HtmlWebpackPlugin({
+                    inject: true,
+                    scriptLoading: "blocking",
+                    template: `/src/${page}.html`,
+                    filename: `${page}.html`,
+                    chunks: [page],
+                })
+        )
+    ),
     output: {
-        filename: "main.js",
+        filename: "[name].js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
